1983e27e29c941009ce6f3d41090c5f137f61971
ithemask <ai77xxxx@gmail.com>
2023-05-05 20:43:13 +0300

    Add brain_games/games/gcd.py
    Implement GCD game

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,18 @@
+import random
+from math import gcd
+
+
+task = 'Find the greatest common divisor of given numbers.'
+
+
+def get_question():
+    # get random numbers
+    num_1 = random.randint(0, 99)
+    num_2 = random.randint(0, 99)
+    return f'{num_1} {num_2}'
+
+
+def get_correct_answer(question):
+    # get greatest common divisor
+    num_pair = question.split(' ')
+    return str(gcd(int(num_pair[0]), int(num_pair[1])))
