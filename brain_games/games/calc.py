8d8344139eb8b9f041597b7ab24d173be65de43a
ithemask <ai77xxxx@gmail.com>
2023-05-22 21:45:06 +0300

    Update calc.py module
    Fix variable naming

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -10,8 +10,8 @@ def get_question():
     # get random expression
     num_1 = random.randint(RANGE_START, RANGE_END)
     num_2 = random.randint(RANGE_START, RANGE_END)
-    sign = random.choice(['+', '-', '*'])
-    return f'{num_1} {sign} {num_2}'
+    operation = random.choice(['+', '-', '*'])
+    return f'{num_1} {operation} {num_2}'
 
 
 def get_correct_answer(question):
