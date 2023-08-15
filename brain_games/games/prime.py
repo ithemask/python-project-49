3d49555aea239d3a3a3dda426c7c3ab7377691d1
ithemask <ai77xxxx@gmail.com>
2023-08-15 11:25:22 +0300

    Update prime.py module

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,23 +6,30 @@ RANGE_START = 0
 RANGE_END = 99
 
 
-def get_question_and_answer():
-    # get random number
-    question = random.randint(RANGE_START, RANGE_END)
+def is_prime(number):
     # is prime number (using 'Sieve of Eratosthenes' algorithm)
-    if question < 2:
-        answer = 'no'
-        return (question, answer)
-    sequence = list(range(2, question + 1))
+    if number < 2:
+        return False
+    sequence = list(range(2, number + 1))
     prime_div_i = 0
     prime_div = sequence[prime_div_i]
-    while prime_div ** 2 <= question:
+    while prime_div ** 2 <= number:
         for i, elem in enumerate(sequence):
             if elem != prime_div and elem % prime_div == 0:
                 sequence.pop(i)
         prime_div_i += 1
         prime_div = sequence[prime_div_i]
-    if question in sequence:
+    if number in sequence:
+        return True
+    else:
+        return False
+
+
+def get_question_and_answer():
+    # get random number
+    question = random.randint(RANGE_START, RANGE_END)
+    # is prime number
+    if is_prime(question):
         answer = 'yes'
     else:
         answer = 'no'
