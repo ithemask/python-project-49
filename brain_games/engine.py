4da3950653e4133b46c8bf0ee2b7f2c5d5177d2e
ithemask <ai77xxxx@gmail.com>
2023-08-15 13:35:07 +0300

    Update engine.py module
    Update run func

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -8,16 +8,14 @@ def run(game):
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? ')
     print(f'Hello, {username}!\n{game.get_task()}')
-    rounds_left = ROUNDS_COUNT
-    while rounds_left:
-        question_and_answer = game.get_question_and_answer()
-        print(f'Question: {question_and_answer[0]}')
-        answer = prompt.string('Your answer: ')
-        if answer != question_and_answer[1]:
-            print(f"'{answer}' is wrong answer ;(. Correct answer "
-                  f"was '{question_and_answer[1]}'.\n"
+    for _ in range(ROUNDS_COUNT):
+        question, answer = game.get_question_and_answer()
+        print(f'Question: {question}')
+        user_answer = prompt.string('Your answer: ')
+        if user_answer != answer:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer "
+                  f"was '{answer}'.\n"
                   f"Let's try again, {username}!")
             return
         print('Correct!')
-        rounds_left -= 1
     print(f'Congratulations, {username}!')
