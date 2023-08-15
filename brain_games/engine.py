c81b288a8bcd30876e5b7605c6e1cf8f399354f8
ithemask <ai77xxxx@gmail.com>
2023-08-15 12:46:10 +0300

    Update engine.py module
    Update run func

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -5,13 +5,12 @@ TRIES_TO_WIN = 3
 
 
 def run(game):
-    exec(f'from .games.{game} import TASK, get_question_and_answer', globals())
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? ')
-    print(f'Hello, {username}!\n{TASK}')
+    print(f'Hello, {username}!\n{game.get_task()}')
     tries_left = TRIES_TO_WIN
     while tries_left:
-        question_and_answer = get_question_and_answer()
+        question_and_answer = game.get_question_and_answer()
         print(f'Question: {question_and_answer[0]}')
         answer = prompt.string('Your answer: ')
         if answer != question_and_answer[1]:
