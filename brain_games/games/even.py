f593060f1f92eefb61feb10843823d3b7d6f4e37
ithemask <ai77xxxx@gmail.com>
2023-08-15 11:10:33 +0300

    Update even.py module

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -6,11 +6,17 @@ RANGE_START = 0
 RANGE_END = 99
 
 
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+
 def get_question_and_answer():
     # get random number
     question = random.randint(RANGE_START, RANGE_END)
     # is even number
-    if question % 2 == 0:
+    if is_even(question):
         answer = 'yes'
     else:
         answer = 'no'
