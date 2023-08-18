abd326f5d7861b56919d936f33bb77903bee27ed
ithemask <ai77xxxx@gmail.com>
2023-08-18 14:34:39 +0300

    Update progression.py module

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,26 +1,24 @@
 import random
 
 
-LEN_RANGE_START = 5
-LEN_RANGE_END = 10
-NUM_RANGE_START = 0
-NUM_RANGE_END = 99
+LEN_RANGE = {'start': 5, 'end': 10}
+START_RANGE = {'start': 0, 'end': 100}
+STEP_RANGE = {'start': 1, 'end': 10}
 
 
 def get_task():
     return 'What number is missing in the progression?'
 
 
-def get_progression():
-    length = random.randint(LEN_RANGE_START, LEN_RANGE_END)
-    start = random.randint(NUM_RANGE_START, NUM_RANGE_END - length)
-    stop = random.randint(start + length, NUM_RANGE_END)
-    step = int((stop - start) / length)
-    return list(map(str, range(start, stop, step)))[:length]
+def get_progression(length, start, step):
+    return list(map(str, range(start, start + step * length, step)))
 
 
 def get_question_and_answer():
-    progression = get_progression()
+    length = random.randint(LEN_RANGE['start'], LEN_RANGE['end'])
+    start = random.randint(START_RANGE['start'], START_RANGE['end'])
+    step = random.randint(STEP_RANGE['start'], STEP_RANGE['end'])
+    progression = get_progression(length, start, step)
     miss_num_i = random.randint(0, len(progression) - 1)
     miss_num = progression[miss_num_i]
     progression[miss_num_i] = '..'
